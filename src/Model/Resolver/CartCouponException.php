3287bd4c535cfec41e526ad363d14d4bbc47c9b1
Mihails Popovs <mihails.popovs@scandiweb.com>
2019-09-13 10:13:10 +0300

    Made CartCouponException ClientAware, so that Magento would display actual message and not Internal Server Error

--- src/Model/Resolver/CartCouponException.php
+++ src/Model/Resolver/CartCouponException.php
@@ -7,8 +7,10 @@ namespace ScandiPWA\QuoteGraphQl\Model\Resolver;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Phrase;
 
-class CartCouponException extends LocalizedException
+class CartCouponException extends LocalizedException implements \GraphQL\Error\ClientAware
 {
+    const EXCEPTION_CATEGORY = 'cart-coupon';
+
     /**
      * Initialize object
      *
@@ -20,5 +22,20 @@ class CartCouponException extends LocalizedException
     {
         parent::__construct($phrase, $cause, $code);
     }
-}
 
+    /**
+     * @inheritdoc
+     */
+    public function isClientSafe() : bool
+    {
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getCategory() : string
+    {
+        return self::EXCEPTION_CATEGORY;
+    }
+}
