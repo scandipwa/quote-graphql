630f88377dad949c061715813a15affb7924ecf1
Andrii Antoniuk <104421198+Andrii-Antoniuk@users.noreply.github.com>
2023-06-22 09:58:40 +0200

    Added more error handling for the GetCartForUser

--- src/Model/Cart/GetCartForUser.php
+++ src/Model/Cart/GetCartForUser.php
@@ -1,26 +1,22 @@
 <?php
+
 /**
- * ScandiPWA - Progressive Web App for Magento
- *
- * Copyright © Scandiweb, Inc. All rights reserved.
- * See LICENSE for license details.
- *
- * @license OSL-3.0 (Open Software License ("OSL") v. 3.0)
- * @package scandipwa/quote-graphql
- * @link    https://github.com/scandipwa/quote-graphql
+ * @category CAG Ateles
+ * @package Ateles\DogmanPWA\QuoteGraphQL\Model;
+ * @author Dzemal Becirevic <dzemal.becirevic@cag.se> & Andrii Antoniuk <andrii.antoniuk@cag.se>
+ * @copyright Copyright (c) 2023 CAG Ateles AB
+ * @license http://opensource.org/licenses/OSL-3.0 The Open Software License 3.0 (OSL-3.0)
  */
 
-declare(strict_types=1);
+namespace Ateles\DogmanPWA\QuoteGraphQL\Model;
 
-namespace ScandiPWA\QuoteGraphQl\Model\Cart;
-
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\GraphQl\Exception\GraphQlAuthorizationException;
+use Magento\Framework\GraphQl\Exception\GraphQlInputException;
+use Magento\Framework\GraphQl\Exception\GraphQlNoSuchEntityException;
 use Magento\Quote\Model\Quote;
 use Magento\QuoteGraphQl\Model\Cart\GetCartForUser as SourceGetCartForUser;
 
-/**
- * Class GetCartForUser
- * @package ScandiPWA\QuoteGraphQl\Model\Cart
- */
 class GetCartForUser extends SourceGetCartForUser
 {
     /**
@@ -28,8 +24,12 @@ class GetCartForUser extends SourceGetCartForUser
      * translated and removed
      */
     protected const STOCK_ERRORS_TO_REMOVE = [
-        'stock' => 'Some of the products are out of stock.',
-        'qty' => 'There are no source items with the in stock status'
+        'stock' => ['Some of the products are out of stock.'],
+        'qty' => [
+            'There are no source items with the in stock status',
+            'This product(s) is out of stock.',
+            'This product is out of stock.'
+        ]
     ];
 
     /**
@@ -44,18 +44,22 @@ class GetCartForUser extends SourceGetCartForUser
      * @throws GraphQlInputException
      * @throws GraphQlNoSuchEntityException
      */
-
     public function execute(string $cartHash, ?int $customerId, int $storeId): Quote
     {
         $cart = parent::execute($cartHash, $customerId, $storeId);
-        /* For Magento, those errors are used for notification.
-         But for PWA, those errors prevent working with cart data,
-         and out_of_stock status is processed based on product fields, not stock error
-         Therefore, the error is removed if it concerns 'Out of stock item in cart'
-         */ 
 
-        foreach (self::STOCK_ERRORS_TO_REMOVE as $type => $message) {
-            $cart->removeMessageByText($type, __($message));
+        /* TIC-2306-0118
+         * ScandiPWA does by default try to remove stock errors from cart
+         * but since they do not manage translated error messages the code
+         * can not remove messages in for example Swedish (sv_SE).
+         *
+         * We also need to remove `qty` stock messages as well since SPWA handles
+         * stock messages from the product.
+         */
+        foreach (self::STOCK_ERRORS_TO_REMOVE as $type => $messages) {
+            foreach ($messages as $message) {
+                $cart->removeMessageByText($type, __($message));
+            }
         }
 
         return $cart;
